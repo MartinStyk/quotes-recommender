06271d6de89c93e473f0ea0ddb797784689f6fbb
Dávid Lupták <luptak.david@gmail.com>
2017-10-03 16:17:34 +0200

    Pop up notifications just once. Global handling of ActiveRecord::RecordNotFound exceptions. Keep running home#index page also if there is no record in db.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :show_alert
+
+  private
+
+  def show_alert
+    flash[:danger] = 'The object you tried to access does not exist'
+    redirect_to action: :index
+  end
 end
