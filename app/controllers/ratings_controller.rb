2358b6d2c93bcbe03f144cfcb1b93bbf8b5c43f3
Martin Styk <martin.styk@gmail.com>
2017-10-15 10:46:51 +0200

    Start rating working

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,32 +1,44 @@
 class RatingsController < ApplicationController
-  before_action :set_rating, only: :update
+  # before_action :set_rating, only: :update
 
-  # def create
-  #   @quote = Quote.find(params[:id])
-  #   puts quote
-  #   puts current_user.id
-  #   @rating = Rating.create(quote_id: @quote.id, user_id: current_user.id, user_rating: params[:user_rating])
-  # end
+  def update
+    @quote = Quote.find(params[:id])
+    @rating = Rating.create(quote_id: @quote.id, user_id: current_user.id, user_rating: params[:user_rating])
+
+    respond_to do |format|
+      if @rating
+        format.html {redirect_to root_path, notice: 'Rating was successfully updated.'}
+        format.json {render :show, status: :ok, location: root_path}
+        format.js
+      else
+        format.html {redirect_to root_path, notice: 'Rating was NOT successfully updated.'}
+        format.json {render json: @rating.errors, status: :unprocessable_entity}
+      end
+    end
+
+  end
 
   # Like a scaffold for ratings update method
   # PATCH/PUT
-  def update
-    # @rating = Rating.new(rating_params)
-
-    respond_to do |format|
-      if @rating.update(rating_params)
-        format.html { redirect_to root_path, notice: 'Rating was successfully updated.' }
-        format.json { render :show, status: :ok, location: root_path }
-        format.js
-      else
-        format.html { redirect_to root_path, notice: 'Rating was NOT successfully updated.' }
-        format.json { render json: @rating.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   # @rating = Rating.new(rating_params)
+  #   p(@rating)
+  #
+  #   respond_to do |format|
+  #     if @rating.update(rating_params)
+  #       p(@rating)
+  #       format.html { redirect_to root_path, notice: 'Rating was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: root_path }
+  #       format.js
+  #     else
+  #       format.html { redirect_to root_path, notice: 'Rating was NOT successfully updated.' }
+  #       format.json { render json: @rating.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   private
-
+  #
   def set_rating
     @rating = Rating.find(params[:id])
   end
