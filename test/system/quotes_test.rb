c413ebe85372bded035ca1d8bd7ee435e4b69783
Martin Styk <martin.styk@gmail.com>
2017-10-16 20:00:18 +0200

    removed tests

--- test/system/quotes_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class QuotesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit quotes_url
-  #
-  #   assert_selector "h1", text: "Quote"
-  # end
-end
