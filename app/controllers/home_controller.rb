675667f1576642e35c8adff9773752a10cbab199
Dávid Lupták <luptak.david@gmail.com>
2017-10-16 19:25:03 +0200

    Stars rating working; enhanced

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,16 @@
 class HomeController < ApplicationController
   def index
     @quote = Quote.offset(rand(Quote.count)).first
-    # Initialize a @rating
-    @rating = Rating.new
 
-    # If there is a singed in user, let's create the @rating
     if user_signed_in? && current_user
-      @rating = Rating.create!(quote_id: @quote.id,
-                               user_id: current_user.id,
-                               # Do we need suggested_rating at all?
-                               suggested_rating: 3)
-      @quote.ratings << @rating
-      puts 'rating with id ' + @rating.id.to_s + ' created.'
+      @rating = Rating.find_or_initialize_by(quote_id: @quote.id,
+                                             user_id: current_user.id)
+      if @rating.user_rating.nil?
+        @rating.user_rating = 0
+      end
+    else
+      @rating = Rating.new
+      @rating.user_rating = 0
     end
-    @rating
   end
 end
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,16 @@
 class RatingsController < ApplicationController
-  # before_action :set_rating, only: :update
-
   def update
+    unless current_user
+      redirect_to root_path
+      return
+    end
+
     @quote = Quote.find(params[:id])
-    @rating = Rating.create(quote_id: @quote.id, user_id: current_user.id, user_rating: params[:user_rating])
+    @rating = Rating.find_or_create_by!(quote_id: @quote.id, user_id: current_user.id)
 
     respond_to do |format|
-      if @rating
-        format.html {redirect_to root_path, notice: 'Rating was successfully updated.'}
+      if @rating.update(user_rating: params[:user_rating])
+        format.html
         format.json {render :show, status: :ok, location: root_path}
         format.js
       else
@@ -16,34 +19,5 @@ class RatingsController < ApplicationController
       end
     end
 
-  end
-
-  # Like a scaffold for ratings update method
-  # PATCH/PUT
-  # def update
-  #   # @rating = Rating.new(rating_params)
-  #   p(@rating)
-  #
-  #   respond_to do |format|
-  #     if @rating.update(rating_params)
-  #       p(@rating)
-  #       format.html { redirect_to root_path, notice: 'Rating was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: root_path }
-  #       format.js
-  #     else
-  #       format.html { redirect_to root_path, notice: 'Rating was NOT successfully updated.' }
-  #       format.json { render json: @rating.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  private
-  #
-  def set_rating
-    @rating = Rating.find(params[:id])
-  end
-
-  def rating_params
-    params.require(:rating).permit(:quote_id, :user_id, :user_rating)
   end
 end
