9227c9d40e6816016c1668b47683d92e8a30d354
Martin Styk <martin.styk@gmail.com>
2017-11-01 21:19:22 +0100

    Performance tuning in content based service

--- app/services/content_based_binary_recommender_service.rb
+++ app/services/content_based_binary_recommender_service.rb
@@ -30,29 +30,52 @@ class ContentBasedBinaryRecommenderService < RecommenderService
 
     score_board = Hash.new
 
-    quotes = Quote.all - @user.viewed_quotes.map(&:quote)
+    # consider only unseen quotes
+    user_viewed_quotes = @user.viewed_quotes.map(&:quote)
 
+    # categories user have already seen and rated quotes in them
     user_preferred_categories = UserCategoryPreference.where(user_id: @user.id).to_a.map {|cat| [cat.id, cat.preference]}.to_h
-    quotes_in_category = Category.all.map {|c| [c.id, c.quotes.size]}.to_h
 
-    size = user_preferred_categories.size
-    
-    quotes.each do |quote|
-      score = 0
-      quote_categories_length = quote.categories.length
-      quote.categories.each do |category|
+    # mapping category -> all quotes in categories, for performance reasons we need this only for quotes already rated by user
+    category_quotes = Category.where(:id => user_preferred_categories.keys).map {|c| [c.id, c.quotes.to_a]}.to_h
 
-        current_category_preference = user_preferred_categories[category.id]
-        unless current_category_preference.nil?
-          score += current_category_preference * 1/ Math.sqrt(quote_categories_length) *
-              Math.log10(quotes_size / quotes_in_category[category.id])
+    # TODO based on join table get mapping quote -> number of quote categories
+    # mapping category -> all quotes in categories, for performance reasons we need this only for quotes already rated by user
+    #quotes_categories = Quote.where(:categories => UserCategoryPreference.where(user_id: @user.id).map(&:category)).map {|c| [c.id, c.quotes.to_a]}.to_h
+
+    # for each quote which belongs to categories user has already preference, and user has not seen the
+    # quote before, compute quote ranking as a sum(quote_in_category_ratio * user_preference_for_given_category * IDF)
+    user_preferred_categories.each do |cat_id, cat_preferrence|
+      quotes_in_category = category_quotes[cat_id]
+      considered_quotes = quotes_in_category - user_viewed_quotes
+      considered_quotes.each do |quote|
+        if score_board[quote].nil?
+          score_board[quote] = 0
         end
-
+        # TODO use mapping quote -> number of quote categories instead of  quote.categories.size
+        score_board[quote] += cat_preferrence * 1/ Math.sqrt(quote.categories.size) *
+            Math.log10(quotes_size / quotes_in_category.size)
       end
-      score_board[quote] = score
-
     end
+
+    # choose the best one
     score_board.key(score_board.values.max)
+
+    # quotes.each do |quote|
+    #   score = 0
+    #   quote_categories_length = quote.categories.length
+    #   quote.categories.each do |category|
+    #
+    #     current_category_preference = user_preferred_categories[category.id]
+    #     unless current_category_preference.nil?
+    #       score += current_category_preference * 1/ Math.sqrt(quote_categories_length) *
+    #           Math.log10(quotes_size / quotes_in_category[category.id])
+    #     end
+    #
+    #   end
+    #   score_board[quote] = score
+    #
+    # end
 
   end
 end
