1462962e61b8357563539816c7170c6d93e85810
Martin Styk <martin.styk@gmail.com>
2017-11-04 21:52:31 +0100

    Random recommender returns unseen quote

--- app/services/random_recommender_service.rb
+++ app/services/random_recommender_service.rb
@@ -1,12 +1,11 @@
-# require 'recommender_service'
-
 class RandomRecommenderService < RecommenderService
   def initialize(params)
     super(params)
   end
 
+  # Returns random unseen quote
   def recommend_next
-    # TODO
-    @quote = Quote.offset(rand(Quote.count)).first
+      unseen_quotes = Quote.all.pluck(:id) - @user.viewed_quotes.pluck(:quote_id)
+      Quote.find unseen_quotes.sample
   end
 end
