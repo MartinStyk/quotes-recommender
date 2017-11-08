e6b7f1b088e5dfb243a6d7322fa327c7b2984f76
Dávid Lupták <luptak.david@gmail.com>
2017-11-08 11:44:04 +0100

    Clean Openshift deployment attempts left-overs

--- migrate-database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Executing bundle exec 'rake db:migrate' ..."
-RAILS_ENV=production bundle exec rake db:migrate
-echo "Executing bundle exec 'rake db:seed' ..."
-RAILS_ENV=production bundle exec rake db:seed
