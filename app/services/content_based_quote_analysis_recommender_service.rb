43b723f92af21056184181787a9ccbae442b5448
Martin Styk <martin.styk@gmail.com>
2017-11-09 22:59:29 +0100

    Finished recommendation logic based on quote text lenght analysis

--- app/services/content_based_quote_analysis_recommender_service.rb
+++ app/services/content_based_quote_analysis_recommender_service.rb
@@ -4,54 +4,99 @@ class ContentBasedQuoteAnalysisRecommenderService < RecommenderService
     super(params)
   end
 
+  # Recommendation based on number of words and length of words of quote.
+  #
+  # User profile for these attributes is computed in RatingsController
+  #
+  # Build score board of all quotes based on these attributes.
+  # Score of quote is determined separately for quote length (number of words) and average quote word lenght
+  #
+  # score for quote length = user_preference_for_quote_length * log_10(number_of_quotes/ number_of_quotes_of_this_length)
+  # score for avg word length = user_preference_for_quote_length * log_10(number_of_quotes/ number_of_quotes_of_this_length)
+  #
+  # In final decision, user preferenco for both parameters are taken with same weight.
+  # If we don't have user preference profile, we return random unseen quote
   def recommend_next
     # quote.id -> expectation on how much user likes this quote
-    score_board = {}
+    score_board_quote_length = {}
+    score_board_word_length = {}
 
     # all quotes
-    all_quotes = Quote.all.to_a
+    all_quotes = Quote.all.pluck(:id)
 
     # ids of all quotes current user has already seen
-    user_viewed_quotes = @user.viewed_quotes.to_a
+    user_viewed_quotes = @user.viewed_quotes.pluck(:quote_id)
+
+    # quote_id => quote length
+    quote_length = Quote.all.pluck(:id, :length).to_h
+
+    # quote_id => quote word avg legth
+    quote_word_length = Quote.all.pluck(:id, :word_avg_length).to_h
 
     # length -> users preference for given length
     user_preferred_quote_lengths = UserQuoteLengthPreference.where(user_id: @user.id)
-                                                      .pluck(:length, :preference)
-                                                      .to_h
+                                       .pluck(:length, :preference).to_h
+
     # length -> number quotes of given length
     quotes_by_quote_length = Quote.group(:length).count
 
     # word_avg_length -> users preference for given word_avg_length
     user_preferred_word_lengths = UserWordLengthPreference.where(user_id: @user.id)
-                                        .pluck(:length, :preference)
-                                        .to_h
+                                      .pluck(:length, :preference)
+                                      .to_h
     # word_avg_length -> number quotes of given length
     quotes_by_word_avg_length = Quote.group(:word_avg_length).count
 
-    (all_quotes - user_viewed_quotes).each do |quote|
-      score_board[quote] = 0 if score_board[quote].nil?
-      # sum of preference * IDF for both quote length and avg word length
-      score_board[quote] += user_preferred_quote_lengths.fetch(quote.length, 0) *
-          Math.log10(all_quotes.size / quotes_by_quote_length[quote.length]) +
-         user_preferred_word_lengths.fetch(quote.word_avg_length, 0) *
-          Math.log10(all_quotes.size / quotes_by_word_avg_length[quote.word_avg_length])
+    (all_quotes - user_viewed_quotes).each do |quote_id|
+
+      # quote length
+      score_board_quote_length[quote_id] = 0 if score_board_quote_length[quote_id].nil?
+      # sum of preference * IDF for both quote length
+      score_board_quote_length[quote_id] += user_preferred_quote_lengths.fetch(quote_length[quote_id], 0) *
+          Math.log10(all_quotes.size / quotes_by_quote_length[quote_length[quote_id]])
+
+      # quote word average length
+      score_board_word_length[quote_id] = 0 if score_board_word_length[quote_id].nil?
+      # sum of preference * IDF for both quote word average length
+      score_board_word_length[quote_id] += user_preferred_word_lengths.fetch(quote_word_length[quote_id], 0) *
+          Math.log10(all_quotes.size / quotes_by_word_avg_length[quote_word_length[quote_id]])
+
     end
 
+    normalize score_board_word_length
+    normalize score_board_quote_length
+
+    merged_score_board = score_board_quote_length.merge(score_board_word_length) {|quote, val1, val2| val1 + val2}
+    final_score_board = merged_score_board.sort_by {|key, value| value}.reverse.to_h
+
+
     # choose the best quote to display
-    best_quote = if !score_board.empty? && score_board.values.max > 0
-                      score_board.key(score_board.values.max)
-                    end
+    best_quote_id = unless final_score_board.empty?
+                   final_score_board.keys[0]
+                 end
 
     # if we dont know users preference, return random unseen quote
     # this happens when there is no quote rating or no unseen quote
-    if best_quote.nil?
-      all_quotes = Quote.all.pluck(:id)
+    if best_quote_id.nil?
       # Consider all quotes as unseen quotes if the user has already viewed all quotes
       unseen_quotes = (all_quotes - user_viewed_quotes).empty? ? all_quotes : (all_quotes - user_viewed_quotes)
       Quote.find unseen_quotes.sample
     else
       # return best result
-      best_quote
+      Quote.find best_quote_id
+    end
+  end
+
+
+  private
+
+  # All scores normalized to value (0,1)
+  def normalize(score_board)
+    max = score_board.values.max
+    min = score_board.values.min
+
+    score_board.each do |key, value|
+      score_board[key] = (value - min) / (max - min)
     end
   end
 end
